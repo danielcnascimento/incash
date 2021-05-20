73a363fc58368ea36922eb2ed8a700d392d285b2
danielcnascimento <ddaniel.nascimento@gmail.com>
2021-05-20 10:34:01 -0300

    added styled on modal; added form style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,21 @@
 import logoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
-export function Header() {
+interface IHeaderProps {
+  onOpenNewTransactionModa: () => void;
+}
+
+export function Header({onOpenNewTransactionModa}:IHeaderProps) {
+
+
     return (
       <Container>
         <Content >    
           <img src={logoImg} alt="" />
-          <button type="button">
+          <button type="button" onClick={onOpenNewTransactionModa} >
             Nova transação
           </button>
+        
         </Content>
       </Container>
     )
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -51,4 +51,41 @@ button {
   opacity: 0.6;
 }
 
+.react-modal-overlay{
+  background: rgba(0,0,0, 0.5);
+
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.react-modal-content{
+  max-width: 576px;
+  width: 100%;
+  background: var(--background);
+  padding: 3rem;
+  position: relative;
+  border-radius: 0.24rem;
+}
+
+.react-modal-close{
+  position: absolute;
+  right: 1.5rem;
+  top: 1.5rem;
+  background: transparent;
+  border: 0;
+
+  transition: filter 0.2s;
+
+  &:hover {
+    filter: brightness(0.7);
+  }
+
+}
 `;
